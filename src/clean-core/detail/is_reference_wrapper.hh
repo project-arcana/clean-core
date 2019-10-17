21805446d773377fec0cf0b1037de9e534d34119
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-10-17 08:19:51 +0200

    Fix typo

--- src/clean-core/detail/is_reference_wrapper.hh
+++ src/clean-core/detail/is_reference_wrapper.hh
@@ -8,15 +8,15 @@ namespace cc
 namespace detail
 {
 template <class T>
-struct test_reference_wrappper : std::false_type
+struct test_reference_wrapper : std::false_type
 {
 };
 template <class U>
-struct test_reference_wrappper<std::reference_wrapper<U>> : std::true_type
+struct test_reference_wrapper<std::reference_wrapper<U>> : std::true_type
 {
 };
 } // namespace detail
 
 template <class T>
-constexpr bool is_reference_wrapper = detail::test_reference_wrappper<T>::value;
+constexpr bool is_reference_wrapper = detail::test_reference_wrapper<T>::value;
 }
