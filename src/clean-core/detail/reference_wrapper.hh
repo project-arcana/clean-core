667323b20afdeefcb8ab6e9844cbbfb8ed6615ca
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-10-23 15:49:53 +0200

    Add incomplete cc::tuple

--- src/clean-core/detail/reference_wrapper.hh
+++ src/clean-core/detail/reference_wrapper.hh
@@ -15,8 +15,24 @@ template <class U>
 struct test_reference_wrapper<std::reference_wrapper<U>> : std::true_type
 {
 };
+
+template <class T>
+struct unwrap_reference_wrapper
+{
+    using type = T;
+};
+
+template <class T>
+struct unwrap_reference_wrapper<std::reference_wrapper<T>>
+{
+    using type = T&;
+};
+
 } // namespace detail
 
 template <class T>
 constexpr bool is_reference_wrapper = detail::test_reference_wrapper<T>::value;
+
+template <class T>
+using reference_decay_t = typename detail::unwrap_reference_wrapper<typename std::decay<T>::type>::type;
 }
