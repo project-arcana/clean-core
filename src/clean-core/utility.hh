cd4e3320324447354a8a92f24797514e78f6b850
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-10-30 18:55:31 +0100

    Add nodiscard to min and max

--- src/clean-core/utility.hh
+++ src/clean-core/utility.hh
@@ -3,13 +3,13 @@
 namespace cc
 {
 template <class T>
-constexpr T const& max(T const& a, T const& b)
+[[nodiscard]] constexpr T const& max(T const& a, T const& b)
 {
     return (a < b) ? b : a;
 }
 
 template <class T>
-constexpr T const& min(T const& a, T const& b)
+[[nodiscard]] constexpr T const& min(T const& a, T const& b)
 {
     return (a < b) ? a : b;
 }
