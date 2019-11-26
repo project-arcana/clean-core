b755d37b9e3bca7f546dab151476b51793ffac5f
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-11-26 11:15:58 +0100

    added base64 encode/decode

---
+++ src/clean-core/base64.cc
@@ -0,0 +1,108 @@
+#include "base64.hh"
+
+#include <cctype>
+
+#include <clean-core/span.hh>
+#include <clean-core/string.hh>
+#include <clean-core/vector.hh>
+
+static cc::string_view constexpr base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                                "abcdefghijklmnopqrstuvwxyz"
+                                                "0123456789+/";
+
+static int find_base64_char(unsigned char c)
+{
+    for (auto i = 0; i < int(base64_chars.size()); ++i)
+        if (base64_chars[i] == c)
+            return i;
+    CC_ASSERT(false && "not found");
+    return -1;
+}
+
+static bool is_base64(unsigned char c) { return (isalnum(c) || (c == '+') || (c == '/')); }
+
+cc::string cc::base64_encode(cc::span<cc::byte> data)
+{
+    cc::string ret;
+    int i = 0;
+    int j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+
+    for (auto b : data)
+    {
+        char_array_3[i++] = (unsigned char)b;
+        if (i == 3)
+        {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
+
+            for (i = 0; (i < 4); i++)
+                ret += base64_chars[char_array_4[i]];
+            i = 0;
+        }
+    }
+
+    if (i)
+    {
+        for (j = i; j < 3; j++)
+            char_array_3[j] = '\0';
+
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
+
+        for (j = 0; (j < i + 1); j++)
+            ret += base64_chars[char_array_4[j]];
+
+        while ((i++ < 3))
+            ret += '=';
+    }
+
+    return ret;
+}
+cc::vector<cc::byte> cc::base64_decode(cc::string_view encoded_string)
+{
+    int in_len = encoded_string.size();
+    int i = 0;
+    int j = 0;
+    int in_ = 0;
+    unsigned char char_array_4[4], char_array_3[3];
+    cc::vector<cc::byte> ret;
+
+    while (in_len-- && (encoded_string[in_] != '=') && is_base64(encoded_string[in_]))
+    {
+        char_array_4[i++] = encoded_string[in_];
+        in_++;
+        if (i == 4)
+        {
+            for (i = 0; i < 4; i++)
+                char_array_4[i] = find_base64_char(char_array_4[i]) & 0xff;
+
+            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+            for (i = 0; (i < 3); i++)
+                ret.push_back(std::byte(char_array_3[i]));
+            i = 0;
+        }
+    }
+
+    if (i)
+    {
+        for (j = 0; j < i; j++)
+            char_array_4[j] = find_base64_char(char_array_4[j]) & 0xff;
+
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+
+        for (j = 0; (j < i - 1); j++)
+            ret.push_back(std::byte(char_array_3[j]));
+    }
+
+    return ret;
+}
