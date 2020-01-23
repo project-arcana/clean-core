4b42f2c0b5e01f665d5ba20047552c28a0bf69c9
Julius Nehring-Wirxel <julius.nehring-wirxel@rwth-aachen.de>
2020-01-23 12:35:26 +0100

    Reworked for simpler usage.

--- src/clean-core/range_access.hh
+++ src/clean-core/range_access.hh
@@ -1,38 +1,53 @@
 #pragma once
 
+#include <type_traits> // std::is_array_v<T>, std::extent_v<T>
+
+#include <clean-core/priority_tag.hh>
+
+namespace cc::detail
+{
+template <class ContainerT>
+constexpr auto begin_impl(ContainerT& c, cc::priority_tag<0>) -> decltype(begin(c))
+{
+    return begin(c);
+}
+
+template <class ContainerT>
+constexpr auto begin_impl(ContainerT& c, cc::priority_tag<1>) -> decltype(c.begin())
+{
+    return c.begin();
+}
+
+template <class ContainerT>
+constexpr auto end_impl(ContainerT& c, cc::priority_tag<0>) -> decltype(end(c))
+{
+    return end(c);
+}
+
+template <class ContainerT>
+constexpr auto end_impl(ContainerT& c, cc::priority_tag<1>) -> decltype(c.end())
+{
+    return c.end();
+}
+}
+
 namespace cc
 {
 template <class ContainerT>
-inline constexpr auto begin(ContainerT& c) -> decltype(c.begin())
+constexpr auto begin(ContainerT& c)
 {
-    return c.begin();
+    if constexpr (std::is_array_v<ContainerT>)
+        return c;
+    else
+        return cc::detail::begin_impl(c, cc::priority_tag<1>{});
 }
 
 template <class ContainerT>
-inline constexpr auto begin(ContainerT const& c) -> decltype(c.begin())
+constexpr auto end(ContainerT& c)
 {
-    return c.begin();
-}
-
-template <class ContainerT>
-inline constexpr auto end(ContainerT& c) -> decltype(c.end())
-{
-    return c.end();
-}
-
-template <class ContainerT>
-inline constexpr auto end(ContainerT const& c) -> decltype(c.end())
-{
-    return c.end();
-}
-
-template <class T, int N>
-inline constexpr T* begin(T (&array)[N]){
-    return array;
-}
-
-template <class T, int N>
-inline constexpr T* end(T (&array)[N]){
-    return array + N;
+    if constexpr (std::is_array_v<ContainerT>)
+        return c + std::extent_v<ContainerT>;
+    else
+        return cc::detail::begin_impl(c, cc::priority_tag<1>{});
 }
 }
