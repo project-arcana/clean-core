7c78b275ed0178e7a645bf20cc69a6204d777778
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2020-04-11 20:01:54 +0200

    executed clang format

--- src/clean-core/detail/xxHash/xxhash.cc
+++ src/clean-core/detail/xxHash/xxhash.cc
@@ -37,7 +37,7 @@
  * xxhash.c instantiates functions defined in xxhash.h
  */
 
-#define XXH_STATIC_LINKING_ONLY   /* access advanced declarations */
-#define XXH_IMPLEMENTATION   /* access definitions */
+#define XXH_STATIC_LINKING_ONLY /* access advanced declarations */
+#define XXH_IMPLEMENTATION      /* access definitions */
 
 #include "xxhash.hh"
