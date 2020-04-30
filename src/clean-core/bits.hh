107eda83ce07e61a34d96f23a0382f39d269b895
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-05-01 01:40:46 +0200

    Add ceil_pow2 and is_pow2 to bits.hh

--- src/clean-core/bits.hh
+++ src/clean-core/bits.hh
@@ -41,6 +41,9 @@ inline int count_trailing_zeros(uint64 v)
     return int(idx);
 }
 
+// WARNING: if the CPU does not support LZCNT, this incorrectly falls back to BSR
+// see https://nextmovesoftware.com/blog/2017/06/06/the-perils-of-using-__lzcnt-with-msvc/
+// Intel Haswell or higher have support, this can be queried with __cpuid
 inline int count_leading_zeros(uint8 v) { return int(__lzcnt16(v) - 8); }
 inline int count_leading_zeros(uint16 v) { return int(__lzcnt16(v)); }
 inline int count_leading_zeros(uint32 v) { return int(__lzcnt(v)); }
@@ -68,4 +71,11 @@ inline int count_leading_zeros(uint64 v) { return __builtin_clzll(v); }
 // returns rounded down logarithm to base 2
 inline uint32 bit_log2(uint32 v) { return uint32(8 * sizeof(uint32) - count_leading_zeros(v) - 1); }
 inline uint64 bit_log2(uint64 v) { return uint64(8 * sizeof(uint64) - count_leading_zeros(v) - 1); }
+
+// ceils to the nearest power of 2
+inline uint32 ceil_pow2(uint32 v) { return uint32(1) << (bit_log2(v - uint32(1)) + 1); }
+inline uint64 ceil_pow2(uint64 v) { return uint64(1) << (bit_log2(v - uint64(1)) + 1); }
+
+inline bool is_pow2(uint32 v) { return ((v & (v - uint32(1))) == 0); }
+inline bool is_pow2(uint64 v) { return ((v & (v - uint64(1))) == 0); }
 }
