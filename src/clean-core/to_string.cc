47ad210ef45db527628c1ac3626a10131b5ed310
Julius Nehring-Wirxel <julius.nehring-wirxel@rwth-aachen.de>
2020-05-09 22:19:45 +0200

    WIP

--- src/clean-core/to_string.cc
+++ src/clean-core/to_string.cc
@@ -95,3 +95,267 @@ cc::string cc::to_string(std::byte value)
     s[1] = hex[v / 16];
     return s;
 }
+
+
+// to_string implementations for builtin types
+// format_spec ::=  [[fill]align][sign]["#"]["0"][width]["." precision][type]
+// fill        ::=  <a character other than '{' or '}'>
+// align       ::=  "<" | ">" | "^"
+// sign        ::=  "+" | "-" | " "
+// width       ::=  integer | "{" arg_id "}"
+// precision   ::=  integer | "{" arg_id "}"
+// type        ::=  int_type | "a" | "A" | "c" | "e" | "E" | "f" | "F" | "g" | "G" | "L" | "p" | "s"
+// int_type    ::=  "b" | "B" | "d" | "o" | "x" | "X"
+namespace
+{
+struct int_fmt_args
+{
+    char fill = 0;                        // anything but '{' or '}', only valid if align valid
+    char align = 0;                       // '<': left | '>': right | '^': center
+    char sign = '-';                      // '+': show both + and - | '-': show only - | ' ': show only -, but leave space if +
+    bool alternative_mode = false;        // alternative display mode
+    bool sign_aware_zero_padding = false; // add leading zeros after the sign +0000123
+    int width = -1;                       //
+    char type = 'd';                      // types 'b': binary | 'B': Binary | 'd': decimal | 'o': octal | 'x':hex | 'X': heX [ | L: locale aware]
+};
+
+
+// todo: generic parse int
+void parse_int_args(int v, cc::string_view fmt_args)
+{
+    auto const is_align = [](char c) { return c == '<' || c == '>' || c == '^'; };
+    auto const is_sign = [](char c) { return c == '+' || c == '-' || c == ' '; };
+    auto const is_digit = [](char c) { return '0' <= c && c <= '9'; };
+
+    auto it = fmt_args.begin();
+    auto const end = fmt_args.end();
+
+    char fill;
+    char const* align = nullptr; // != nullptr if aligned
+    char sign = '-';             // default
+    bool alternative = false;
+    bool sign_aware_zero_padding = false;
+    int width = -1;
+    char type = 'd';
+
+    // fill and alignment
+    if (fmt_args.size() >= 2 && is_align(*(it + 1)))
+    {
+        fill = *it;
+        ++it;
+        align = it;
+        ++it;
+    }
+    // sign
+    if (it != end && is_sign(*it))
+    {
+        sign = *it;
+        ++it;
+    }
+    // alternative mode
+    if (it != end && (*it == '#'))
+    {
+        alternative = true;
+        ++it;
+    }
+    // zero padding and width
+    if (it != end && (*it == '0'))
+    {
+        sign_aware_zero_padding = true;
+        ++it;
+        CC_ASSERT(it != end && is_digit(*it) && "Invalid format string: zero padding must be followed by by width");
+        CC_ASSERT(*it != '0' && "Invalid format string: width can have at most one preceeding zero");
+        width = *it - '0';
+        ++it;
+        while (it != end && is_digit(*it))
+        {
+            width *= 10;
+            width += *it - '0';
+            ++it;
+        }
+    }
+    // no zero padding and with
+    if (it != end && is_digit(*it))
+    {
+        width = *it - '0';
+        ++it;
+        while (it != end && is_digit(*it))
+        {
+            width *= 10;
+            width += *it - '0';
+            ++it;
+        }
+    }
+    // precision does not apply to integer and is therefore skipped
+    // integer types
+    if (it != end)
+    {
+        // todo: locale?
+        CC_ASSERT((*it == 'b' || *it == 'B' || *it == 'd' || *it == 'o' || *it == 'x' || *it == 'X') && "Invalid format string: unsupported type");
+        type = *it;
+        ++it;
+    }
+    CC_ASSERT(it == end && "Invalid format string: invalid integer options");
+
+    // todo: what do with settings?
+}
+
+void parse_float(float, cc::string_view fmt_args)
+{
+    auto const is_align = [](char c) { return c == '<' || c == '>' || c == '^'; };
+    auto const is_sign = [](char c) { return c == '+' || c == '-' || c == ' '; };
+    auto const is_digit = [](char c) { return '0' <= c && c <= '9'; };
+
+    auto it = fmt_args.begin();
+    auto const end = fmt_args.end();
+
+    char fill;
+    char const* align = nullptr; // != nullptr if aligned
+    char sign = '-';             // default
+    bool alternative = false;
+    bool sign_aware_zero_padding = false;
+    int width = -1;
+    char type = 'd';
+    int precision = -1;
+
+    // fill and alignment
+    if (fmt_args.size() >= 2 && is_align(*(it + 1)))
+    {
+        fill = *it;
+        ++it;
+        align = it;
+        ++it;
+    }
+    // sign
+    if (it != end && is_sign(*it))
+    {
+        sign = *it;
+        ++it;
+    }
+    // alternative mode
+    if (it != end && (*it == '#'))
+    {
+        alternative = true;
+        ++it;
+    }
+    // zero padding and width
+    if (it != end && (*it == '0'))
+    {
+        sign_aware_zero_padding = true;
+        ++it;
+        CC_ASSERT(it != end && is_digit(*it) && "Invalid format string: zero padding must be followed by by width");
+        CC_ASSERT(*it != '0' && "Invalid format string: width can have at most one preceeding zero");
+        width = *it - '0';
+        ++it;
+        while (it != end && is_digit(*it))
+        {
+            width *= 10;
+            width += *it - '0';
+            ++it;
+        }
+    }
+    // no zero padding and with
+    if (it != end && is_digit(*it))
+    {
+        width = *it - '0';
+        ++it;
+        while (it != end && is_digit(*it))
+        {
+            width *= 10;
+            width += *it - '0';
+            ++it;
+        }
+    }
+    if (it != end && *it == '.')
+    {
+        ++it;
+        CC_ASSERT(it != end && is_digit(*it) && "Invalid format string: . must be followed by the precision");
+        precision = *it - '0';
+        ++it;
+        while (it != end && is_digit(*it))
+        {
+            precision *= 10;
+            precision += *it - '0';
+            ++it;
+        }
+    }
+
+    // type
+    if (it != end) // todo: locale?
+    {
+        CC_ASSERT((*it == 'a' || *it == 'A' || *it == 'e' || *it == 'E' || *it == 'f' || *it == 'F' || *it == 'g' || *it == 'G') && "Invalid format string: invalid type");
+        type = *it;
+        ++it;
+    }
+    CC_ASSERT(it == end && "Invalid format string: invalid integer options");
+}
+}
+
+// void cc::to_string(cc::string_stream& ss, char value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+// void cc::to_string(cc::string_stream& ss, bool value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+// void cc::to_string(cc::string_stream& ss, char const* value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+// void cc::to_string(cc::string_stream& ss, cc::string_view value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+// void cc::to_string(cc::string_stream& ss, nullptr_t, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+
+// void cc::to_string(cc::string_stream& ss, void* value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+
+// void cc::to_string(cc::string_stream& ss, std::byte value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+
+// void cc::to_string(cc::string_stream& ss, int value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+// void cc::to_string(cc::string_stream& ss, long value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+// void cc::to_string(cc::string_stream& ss, long long value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+// void cc::to_string(cc::string_stream& ss, unsigned int value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+// void cc::to_string(cc::string_stream& ss, unsigned long value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+// void cc::to_string(cc::string_stream& ss, unsigned long long value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+
+// void cc::to_string(cc::string_stream& ss, float value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+// void cc::to_string(cc::string_stream& ss, double value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
+// void cc::to_string(cc::string_stream& ss, long double value, cc::string_view fmt_str)
+//{
+//    // todo
+//}
--- src/clean-core/to_string.hh
+++ src/clean-core/to_string.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <clean-core/string.hh>
+#include <clean-core/string_stream.hh>
 
 namespace cc
 {
@@ -24,4 +25,26 @@ string to_string(unsigned long long value);
 string to_string(float value);
 string to_string(double value);
 string to_string(long double value);
+
+// void to_string(string_stream& ss, char value, string_view fmt_str);
+// void to_string(string_stream& ss, bool value, string_view fmt_str);
+// void to_string(string_stream& ss, char const* value, string_view fmt_str);
+// void to_string(string_stream& ss, string_view value, string_view fmt_str);
+// void to_string(string_stream& ss, nullptr_t, string_view fmt_str);
+
+// void to_string(string_stream& ss, void* value, string_view fmt_str);
+
+// void to_string(string_stream& ss, std::byte value, string_view fmt_str);
+
+// void to_string(string_stream& ss, int value, string_view fmt_str);
+// void to_string(string_stream& ss, long value, string_view fmt_str);
+// void to_string(string_stream& ss, long long value, string_view fmt_str);
+// void to_string(string_stream& ss, unsigned int value, string_view fmt_str);
+// void to_string(string_stream& ss, unsigned long value, string_view fmt_str);
+// void to_string(string_stream& ss, unsigned long long value, string_view fmt_str);
+
+// void to_string(string_stream& ss, float value, string_view fmt_str);
+// void to_string(string_stream& ss, double value, string_view fmt_str);
+// void to_string(string_stream& ss, long double value, string_view fmt_str);
+
 }
