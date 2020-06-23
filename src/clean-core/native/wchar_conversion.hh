a00f447dc8a3c9ef1db96b536f117c011fb0a486
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-06-23 22:42:32 +0200

    Add span overloads to wchar util functions

--- src/clean-core/native/wchar_conversion.hh
+++ src/clean-core/native/wchar_conversion.hh
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <clean-core/span.hh>
+
 namespace cc
 {
 /// converts a wchar_t string to a (UTF-8) char string
@@ -7,8 +9,18 @@ namespace cc
 /// returns amount of characters written to dest
 int widechar_to_char(char* dest, int max_num_dest_chars, wchar_t const* src, int opt_num_src_chars = -1);
 
+inline int widechar_to_char(cc::span<char> dest, wchar_t const* src, int opt_num_src_chars = -1)
+{
+    return widechar_to_char(dest.data(), int(dest.size()), src, opt_num_src_chars);
+}
+
 /// converts a (UTF-8) char string to a wchar_t string
 /// opt_num_src_chars can be specified to stop conversion before '\0' terminateion
 /// returns amount of characters written to dest
 int char_to_widechar(wchar_t* dest, int max_num_dest_chars, char const* src, int opt_num_src_chars = -1);
+
+inline int char_to_widechar(cc::span<wchar_t> dest, char const* src, int opt_num_src_chars = -1)
+{
+    return char_to_widechar(dest.data(), int(dest.size()), src, opt_num_src_chars);
+}
 }
