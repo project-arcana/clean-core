e1fd82dff076ae98223d1789fd97dc23497afe72
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2020-10-01 07:45:14 +0200

    has_op_subscript

--- src/clean-core/has_operator.hh
+++ src/clean-core/has_operator.hh
@@ -10,37 +10,42 @@ namespace detail
 template <class A, class B>
 [[maybe_unused]] static auto test_op_less(int) -> decltype(bool(std::declval<A>() < std::declval<B>()), std::true_type{});
 template <class A, class B>
-[[maybe_unused]] static auto test_op_less(...) -> std::false_type;
+[[maybe_unused]] static auto test_op_less(char) -> std::false_type;
 
 template <class A, class B>
 [[maybe_unused]] static auto test_op_greater(int) -> decltype(bool(std::declval<A>() > std::declval<B>()), std::true_type{});
 template <class A, class B>
-[[maybe_unused]] static auto test_op_greater(...) -> std::false_type;
+[[maybe_unused]] static auto test_op_greater(char) -> std::false_type;
 
 template <class A, class B>
 [[maybe_unused]] static auto test_op_less_or_equal(int) -> decltype(bool(std::declval<A>() <= std::declval<B>()), std::true_type{});
 template <class A, class B>
-[[maybe_unused]] static auto test_op_less_or_equal(...) -> std::false_type;
+[[maybe_unused]] static auto test_op_less_or_equal(char) -> std::false_type;
 
 template <class A, class B>
 [[maybe_unused]] static auto test_op_greater_or_equal(int) -> decltype(bool(std::declval<A>() >= std::declval<B>()), std::true_type{});
 template <class A, class B>
-[[maybe_unused]] static auto test_op_greater_or_equal(...) -> std::false_type;
+[[maybe_unused]] static auto test_op_greater_or_equal(char) -> std::false_type;
 
 template <class A, class B>
 [[maybe_unused]] static auto test_op_equal(int) -> decltype(bool(std::declval<A>() == std::declval<B>()), std::true_type{});
 template <class A, class B>
-[[maybe_unused]] static auto test_op_equal(...) -> std::false_type;
+[[maybe_unused]] static auto test_op_equal(char) -> std::false_type;
 
 template <class A, class B>
 [[maybe_unused]] static auto test_op_not_equal(int) -> decltype(bool(std::declval<A>() != std::declval<B>()), std::true_type{});
 template <class A, class B>
-[[maybe_unused]] static auto test_op_not_equal(...) -> std::false_type;
+[[maybe_unused]] static auto test_op_not_equal(char) -> std::false_type;
 
 template <class A, class B>
 [[maybe_unused]] static auto test_op_left_shift(int) -> decltype(std::declval<A>() << std::declval<B>(), std::true_type{});
 template <class A, class B>
-[[maybe_unused]] static auto test_op_left_shift(...) -> std::false_type;
+[[maybe_unused]] static auto test_op_left_shift(char) -> std::false_type;
+
+template <class A, class B>
+[[maybe_unused]] static auto test_op_subscript(int) -> decltype(std::declval<A>()[std::declval<B>()], std::true_type{});
+template <class A, class B>
+[[maybe_unused]] static auto test_op_subscript(char) -> std::false_type;
 } // namespace detail
 
 template <class A, class B = A>
@@ -56,6 +61,9 @@ constexpr bool has_operator_equal = decltype(detail::test_op_equal<A const&, B c
 template <class A, class B = A>
 constexpr bool has_operator_not_equal = decltype(detail::test_op_not_equal<A const&, B const&>(0))::value;
 
-template <class A, class B = A>
-constexpr bool has_operator_left_shift = decltype(detail::test_op_left_shift<A const&, B const&>(0))::value;
+template <class A, class B>
+constexpr bool has_operator_left_shift = decltype(detail::test_op_left_shift<A, B>(0))::value;
+
+template <class A, class B>
+constexpr bool has_operator_subscript = decltype(detail::test_op_subscript<A, B>(0))::value;
 }
