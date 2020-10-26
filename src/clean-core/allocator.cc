b02af0fc4f772db0cf20015e815f90cf20521fee
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-10-26 21:59:09 +0100

    Reformat bitsquid license, rename macro

--- src/clean-core/allocator.cc
+++ src/clean-core/allocator.cc
@@ -8,10 +8,10 @@
 #include <clean-core/assert.hh>
 #include <clean-core/utility.hh>
 
-// enable debug trace printfs in some allocators
-#define CC_DEBUG_TRACE_SCRATCH_ALLOC 0
+// enable debug trace prints in some allocators
+#define CC_ENABLE_DTRACE_ALLOC 0
 
-#if CC_DEBUG_TRACE_SCRATCH_ALLOC
+#if CC_ENABLE_DTRACE_ALLOC
 #define CC_DTRACE_ALLOC_PRINTF printf
 #define CC_DTRACE_ALLOC_FFLUSH() fflush(stdout)
 #else
@@ -241,20 +241,23 @@ cc::byte* cc::allocator::realloc_request(void* ptr, cc::size_t old_size, cc::siz
     out_received_size = new_min_size;
     return this->realloc(ptr, old_size, new_min_size, align);
 }
-// cc::scratch_allocator is based on the bitsquid foundation ScratchAllocator
-// https://github.com/niklas-ourmachinery/bitsquid-foundation/blob/master/memory.cpp#L142
-// the original implementation contains bugs which are fixed here
-//
-// original license (MIT):
-//
-// Copyright (C) 2012 Bitsquid AB
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+/*
+ * cc::scratch_allocator is based on the bitsquid foundation ScratchAllocator
+ * https://github.com/niklas-ourmachinery/bitsquid-foundation/blob/master/memory.cpp#L142
+ * the original implementation contains bugs which are fixed here
+ *
+ * original license (MIT):
+ *
+ * Copyright (C) 2012 Bitsquid AB
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 cc::byte* cc::scratch_allocator::alloc(cc::size_t size, cc::size_t align)
 {
