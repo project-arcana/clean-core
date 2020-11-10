1d37b2a55e8955f99ceea7960e282f757446c54f
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-11-10 17:05:13 +0100

    Simplify stringhash

--- src/clean-core/stringhash.hh
+++ src/clean-core/stringhash.hh
@@ -1,30 +1,20 @@
 #pragma once
 
-#include <clean-core/typedefs.hh>
+#include <clean-core/hash_combine.hh>
 
 namespace cc
 {
-/// calculate a string hash from a string literal at compile time
-template <size_t N>
-constexpr hash_t stringhash(const char (&str)[N], size_t prime = 31, size_t length = N - 1)
+constexpr hash_t stringhash(char const* s)
 {
-    // ref https://xueyouchao.github.io/2016/11/16/CompileTimeString/
-    return (length <= 1) ? str[0] : (prime * stringhash(str, prime, length - 1) + str[length - 1]);
-}
-
-/// usage:
-static_assert(stringhash("value") != 0);
-
-/// calculate a string hash from a runtime string
-inline hash_t stringhash_runtime(char const* str, size_t prime = 31)
-{
-    if (str == nullptr)
+    if (!s)
         return 0;
-    hash_t hash = *str;
-    for (; *(str + 1) != 0; str++)
+
+    hash_t h = hash_combine();
+    while (*s)
     {
-        hash = prime * hash + *(str + 1);
+        h = hash_combine(h, hash_t(*s));
+        s++;
     }
-    return hash;
+    return h;
 }
 }
