61b1aa95916b76f35fb329842e3bc50e12049731
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-11-10 15:20:24 +0100

    Add atomic_linked_pool

---
+++ src/clean-core/atomic_linked_pool.cc
@@ -0,0 +1,31 @@
+#include "atomic_linked_pool.hh"
+
+// Radix Sort implementation from https://github.com/983/RadixSort
+// License: "Unlicense" (public domain)
+
+namespace
+{
+CC_FORCE_INLINE void radix_sort_pass(uint32_t const* __restrict src, uint32_t* __restrict dst, size_t n, size_t shift)
+{
+    size_t next_index = 0, index[256] = {0};
+    for (size_t i = 0; i < n; i++)
+        index[(src[i] >> shift) & 0xff]++;
+    for (size_t i = 0; i < 256; i++)
+    {
+        size_t const count = index[i];
+        index[i] = next_index;
+        next_index += count;
+    }
+    for (size_t i = 0; i < n; i++)
+        dst[index[(src[i] >> shift) & 0xff]++] = src[i];
+}
+}
+
+
+void cc::radix_sort(uint32_t* __restrict a, uint32_t* __restrict temp, size_t n)
+{
+    radix_sort_pass(a, temp, n, 0 * 8);
+    radix_sort_pass(temp, a, n, 1 * 8);
+    radix_sort_pass(a, temp, n, 2 * 8);
+    radix_sort_pass(temp, a, n, 3 * 8);
+}
