280889e7bd2a67846e164b91dcda0b18ebb5c4e9
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2023-05-04 20:46:34 +0200

    indices_of with integral types

--- src/clean-core/indices_of.hh
+++ src/clean-core/indices_of.hh
@@ -18,12 +18,24 @@ namespace cc
 ///   for (size_t i = 0; i != v.size(); ++i)
 ///       use(i);
 ///
+///   // and the same as
+///   for (auto i : cc::indices_of(v.size())(
+///       use(i);
+///
 /// the generated assembly is equivalent for simple loops: https://godbolt.org/z/vvEKno4jT
 /// and can be better when aliasing is involved: https://godbolt.org/z/Tv5ddjvaz
-template <class SizedRange>
-constexpr auto indices_of(SizedRange const& range)
+template <class SizedRangeOrIntegral>
+constexpr auto indices_of(SizedRangeOrIntegral const& range)
 {
-    using T = std::decay_t<decltype(cc::collection_size(range))>;
-    return cc::detail::irange<T>{T(0), cc::collection_size(range)};
+    if constexpr (std::is_integral_v<SizedRangeOrIntegral>)
+    {
+        using T = SizedRangeOrIntegral;
+        return cc::detail::irange<T>{T(0), range};
+    }
+    else
+    {
+        using T = std::decay_t<decltype(cc::collection_size(range))>;
+        return cc::detail::irange<T>{T(0), cc::collection_size(range)};
+    }
 }
 }
