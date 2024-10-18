aad4e4b3459cb959a216e5dc8b8aaba346fd9899
Patric Schmitz <bzk0711@aol.com>
2024-10-18 10:22:40 +0200

    bump xxhash as the old version failed on gcc

--- src/clean-core/hash.xxh3.cc
+++ src/clean-core/hash.xxh3.cc
@@ -1,5 +1,5 @@
 #include "hash.xxh3.hh"
 
-#include <clean-core/detail/xxHash/xxh3.hh>
+#include <clean-core/detail/xxHash/xxhash.hh>
 
-uint64_t cc::make_hash_xxh3(cc::span<const std::byte> data, uint64_t seed) { return XXH3_64bits_withSeed(data.data(), data.size(), seed); }
+uint64_t cc::make_hash_xxh3(cc::span<std::byte const> data, uint64_t seed) { return XXH3_64bits_withSeed(data.data(), data.size(), seed); }
