6d560b7adc53bbe4fa78fe4177de9adb79dcdf0a
Philip Trettner <trettner@shapedcode.com>
2025-06-06 14:40:17 +0200

    cc::bitset for any N, extensive MCT

--- src/clean-core/bitset.hh
+++ src/clean-core/bitset.hh
@@ -7,25 +7,175 @@
 
 namespace cc
 {
+namespace detail
+{
+/// a simple wrapper for W x u64
+/// where only "bit operations" are exposed
+template <int W>
+struct bit_words
+{
+    size_t words[W] = {};
+
+    constexpr bit_words() = default;
+    // NOTE: only lower word!
+    constexpr explicit bit_words(size_t data) { words[0] = data; }
+
+    static constexpr bit_words ones(size_t cnt)
+    {
+        // CC_ASSERT(cnt < W * 8 * sizeof(size_t)); -- not constexpr
+        bit_words w;
+        size_t i = 0;
+
+        while (cnt >= 64)
+        {
+            w.words[i] = size_t(-1);
+            ++i;
+            cnt -= 64;
+        }
+
+        if (cnt > 0)
+            w.words[i] = (size_t(1) << cnt) - 1;
+
+        return w;
+    }
+
+    friend constexpr bit_words operator~(bit_words ws)
+    {
+        for (auto& w : ws.words)
+            w = ~w;
+        return ws;
+    }
+
+    friend constexpr bit_words operator|(bit_words const& ws0, bit_words const& ws1)
+    {
+        bit_words w;
+        for (auto i = 0; i < W; ++i)
+            w.words[i] = ws0.words[i] | ws1.words[i];
+        return w;
+    }
+
+    friend constexpr bit_words operator&(bit_words const& ws0, bit_words const& ws1)
+    {
+        bit_words w;
+        for (auto i = 0; i < W; ++i)
+            w.words[i] = ws0.words[i] & ws1.words[i];
+        return w;
+    }
+
+    friend constexpr bit_words operator^(bit_words const& ws0, bit_words const& ws1)
+    {
+        bit_words w;
+        for (auto i = 0; i < W; ++i)
+            w.words[i] = ws0.words[i] ^ ws1.words[i];
+        return w;
+    }
+
+    friend constexpr bit_words operator>>(bit_words const& ws, int b)
+    {
+        auto w_offset = b / 64;
+        auto w_shift = b - 64 * w_offset;
+        if (w_shift < 0)
+        {
+            w_offset--;
+            w_shift += 64;
+        }
+        CC_ASSERT(0 <= w_shift && w_shift < 64);
+
+        bit_words w;
+        for (auto i = 0; i < W; ++i)
+        {
+            auto wi = i + w_offset;
+            auto wii = wi + 1;
+
+            if (0 <= wi && wi < W)
+                w.words[i] = ws.words[wi] >> w_shift;
+
+            if (w_shift > 0 && 0 <= wii && wii < W)
+                w.words[i] |= ws.words[wii] << (64 - w_shift);
+        }
+        return w;
+    }
+
+    friend constexpr bit_words operator<<(bit_words const& ws, int b) { return ws >> -b; }
+
+    constexpr bit_words& operator|=(bit_words const& ws)
+    {
+        for (auto i = 0; i < W; ++i)
+            words[i] |= ws.words[i];
+        return *this;
+    }
+
+    constexpr bit_words& operator&=(bit_words const& ws)
+    {
+        for (auto i = 0; i < W; ++i)
+            words[i] &= ws.words[i];
+        return *this;
+    }
+
+    constexpr bit_words& operator^=(bit_words const& ws)
+    {
+        for (auto i = 0; i < W; ++i)
+            words[i] ^= ws.words[i];
+        return *this;
+    }
+
+    constexpr bit_words& operator<<=(int b)
+    {
+        *this = *this << b;
+        return *this;
+    }
+
+    constexpr bit_words& operator>>=(int b)
+    {
+        *this = *this >> b;
+        return *this;
+    }
+
+    friend constexpr bool operator==(bit_words const& ws0, bit_words const& ws1)
+    {
+        for (auto i = 0; i < W; ++i)
+            if (ws0.words[i] != ws1.words[i])
+                return false;
+        return true;
+    }
+
+    friend constexpr bool operator!=(bit_words const& ws0, bit_words const& ws1)
+    {
+        for (auto i = 0; i < W; ++i)
+            if (ws0.words[i] != ws1.words[i])
+                return true;
+        return false;
+    }
+};
+} // namespace detail
+
 /// compile-time fixed-size bitset
-/// TODO: select underlying type based on N
+/// TODO: optimize some stuff if N % 64 == 0
 template <size_t N>
 struct bitset
 {
-    static_assert(N <= 64, "TODO: support larger sizes");
+    static_assert(N > 0, "TODO: support zero-sized bitset for compat purposes");
+    using repr_t = detail::bit_words<1 + (N - 1) / (8 * sizeof(size_t))>;
 
     constexpr bitset() = default;
     constexpr explicit bitset(size_t data) : _data(data) {}
+    constexpr explicit bitset(repr_t data) : _data(data) {}
+
+    // ctor
+public:
+    static constexpr bitset zeroes() { return bitset(); }
+    static constexpr bitset ones() { return bitset(data_mask); }
+    static constexpr bitset filled(bool value) { return value ? bitset(data_mask) : bitset(); }
 
     // bit operations
 public:
-    friend constexpr bitset operator~(bitset const& a) { return bitset(~a._data); }
+    friend constexpr bitset operator~(bitset const& a) { return bitset(~a._data & data_mask); }
 
     friend constexpr bitset operator|(bitset const& a, bitset const& b) { return bitset(a._data | b._data); }
     friend constexpr bitset operator&(bitset const& a, bitset const& b) { return bitset(a._data & b._data); }
     friend constexpr bitset operator^(bitset const& a, bitset const& b) { return bitset(a._data ^ b._data); }
     friend constexpr bitset operator<<(bitset const& a, int b) { return bitset((a._data << b) & data_mask); }
-    friend constexpr bitset operator>>(bitset const& a, int b) { return bitset(a._data >> b); }
+    friend constexpr bitset operator>>(bitset const& a, int b) { return bitset((a._data >> b) & data_mask); }
 
     constexpr bitset& operator|=(bitset const& b)
     {
@@ -49,68 +199,66 @@ public:
     }
     constexpr bitset& operator>>=(int b)
     {
-        _data >>= b;
+        _data = (_data >> b) & data_mask; // ensure no 1 in upper bits (for negative b)
         return *this;
     }
 
     friend constexpr bool operator==(bitset const& a, bitset const& b) { return a._data == b._data; }
     friend constexpr bool operator!=(bitset const& a, bitset const& b) { return a._data != b._data; }
-    // TODO: other comparisons?
 
     // properties
 public:
     constexpr size_t size() const { return N; }
     // NOTE: no .data() ! this might get picked up wrongly by contiguous ranges
 
-    constexpr bool any() const { return _data != 0; }
+    constexpr bool any() const { return _data != repr_t{}; }
     constexpr bool all() const { return _data == data_mask; }
 
     constexpr bool is_set(size_t idx) const
     {
         CC_ASSERT(idx < N);
-        return _data & (1 << idx);
+        return _data.words[idx / 64] & (size_t(1) << (idx % 64));
     }
     constexpr bool is_unset(size_t idx) const
     {
         CC_ASSERT(idx < N);
-        return !(_data & (1 << idx));
+        return !(_data.words[idx / 64] & (size_t(1) << (idx % 64)));
     }
 
-    // NOTE: might change depending on N
-    constexpr size_t representation() const { return _data; }
+    constexpr repr_t const& representation() const { return _data; }
 
     // NOTE: currently not settable
     constexpr bool operator[](size_t idx) const
     {
         CC_ASSERT(idx < N);
-        return _data & (1 << idx);
+        return _data.words[idx / 64] & (size_t(1) << (idx % 64));
     }
 
     // methods
 public:
-    constexpr void clear() { _data = 0; }
+    constexpr void clear() { _data = {}; }
 
     constexpr void set(size_t idx)
     {
         CC_ASSERT(idx < N);
-        _data |= 1 << idx;
+        _data.words[idx / 64] |= size_t(1) << (idx % 64);
     }
     constexpr void unset(size_t idx)
     {
         CC_ASSERT(idx < N);
-        _data &= ~(1 << idx);
+        _data.words[idx / 64] &= ~(size_t(1) << (idx % 64));
     }
     constexpr void toggle(size_t idx)
     {
         CC_ASSERT(idx < N);
-        _data ^= 1 << idx;
+        _data.words[idx / 64] ^= size_t(1) << (idx % 64);
     }
 
 private:
     // NOTE: always contains 0 in unused bits
-    size_t _data = 0;
+    repr_t _data = {};
 
-    static constexpr size_t data_mask = (1 << N) - 1;
+    static constexpr repr_t data_mask = repr_t::ones(N);
 };
 
 /// dynamically allocated bitset
@@ -120,4 +268,4 @@ struct bitset<dynamic_size>
 {
     // TODO: implement me
 };
-}
+} // namespace cc
---
+++ tests/bitset.cc
@@ -0,0 +1,282 @@
+#include <nexus/monte_carlo_test.hh>
+
+#include <clean-core/bitset.hh>
+#include <clean-core/indices_of.hh>
+#include <clean-core/set.hh>
+
+// NOTE: bit operations are often subtly wrong, so we really test the whole interface
+
+namespace
+{
+
+template <int N>
+struct reference_set
+{
+    cc::set<int> entries;
+
+    static reference_set zeroes() { return {}; }
+    static reference_set ones()
+    {
+        reference_set s;
+        for (auto i : cc::indices_of(N))
+            s.entries.add(i);
+        return s;
+    }
+    static reference_set filled(bool value)
+    {
+        reference_set s;
+        if (value)
+            for (auto i : cc::indices_of(N))
+                s.entries.add(i);
+        return s;
+    }
+
+    size_t size() const { return N; }
+    bool any() const { return !entries.empty(); }
+    bool all() const { return entries.size() == N; }
+
+    bool is_set(size_t idx) const { return entries.contains(idx); }
+    bool is_unset(size_t idx) const { return !entries.contains(idx); }
+
+    bool operator[](size_t idx) const { return entries.contains(idx); }
+
+    void clear() { entries.clear(); }
+
+    void set(size_t idx) { entries.add(idx); }
+    void unset(size_t idx) { entries.remove(idx); }
+
+    void toggle(size_t idx)
+    {
+        if (entries.contains(idx))
+            entries.remove(idx);
+        else
+            entries.add(idx);
+    }
+
+    friend reference_set operator~(reference_set const& a)
+    {
+        reference_set s;
+        for (auto i : cc::indices_of(N))
+            if (!a.entries.contains(i))
+                s.entries.add(i);
+        return s;
+    }
+
+    friend reference_set operator|(reference_set const& a, reference_set const& b)
+    {
+        reference_set s;
+        for (auto i : cc::indices_of(N))
+            if (a.entries.contains(i) || b.entries.contains(i))
+                s.entries.add(i);
+        return s;
+    }
+
+    friend reference_set operator&(reference_set const& a, reference_set const& b)
+    {
+        reference_set s;
+        for (auto i : cc::indices_of(N))
+            if (a.entries.contains(i) && b.entries.contains(i))
+                s.entries.add(i);
+        return s;
+    }
+
+    friend reference_set operator^(reference_set const& a, reference_set const& b)
+    {
+        reference_set s;
+        for (auto i : cc::indices_of(N))
+            if (a.entries.contains(i) != b.entries.contains(i))
+                s.entries.add(i);
+        return s;
+    }
+
+    friend reference_set operator<<(reference_set const& a, int b)
+    {
+        reference_set s;
+        for (auto i : cc::indices_of(N))
+            if (a.entries.contains(i - b))
+                s.entries.add(i);
+        return s;
+    }
+
+    friend reference_set operator>>(reference_set const& a, int b)
+    {
+        reference_set s;
+        for (auto i : cc::indices_of(N))
+            if (a.entries.contains(i + b))
+                s.entries.add(i);
+        return s;
+    }
+
+    reference_set& operator|=(reference_set const& a)
+    {
+        if (&a == this)
+            return *this;
+
+        for (auto i : a.entries)
+            entries.add(i);
+        return *this;
+    }
+
+    reference_set& operator&=(reference_set const& a)
+    {
+        if (&a == this)
+            return *this;
+
+        for (auto i : cc::indices_of(N))
+            if (!a.entries.contains(i))
+                entries.remove(i);
+        return *this;
+    }
+
+    reference_set& operator^=(reference_set const& a)
+    {
+        cc::set<int> new_entries;
+        for (auto i : cc::indices_of(N))
+            if (entries.contains(i) != a.entries.contains(i))
+                new_entries.add(i);
+        entries = cc::move(new_entries);
+        return *this;
+    }
+
+    reference_set& operator<<=(int b)
+    {
+        cc::set<int> new_entries;
+        for (auto i : cc::indices_of(N))
+            if (entries.contains(i - b))
+                new_entries.add(i);
+        entries = cc::move(new_entries);
+        return *this;
+    }
+
+    reference_set& operator>>=(int b)
+    {
+        cc::set<int> new_entries;
+        for (auto i : cc::indices_of(N))
+            if (entries.contains(i + b))
+                new_entries.add(i);
+        entries = cc::move(new_entries);
+        return *this;
+    }
+
+    bool operator==(reference_set const& b) const { return entries == b.entries; }
+    bool operator!=(reference_set const& b) const { return entries != b.entries; }
+
+    bool operator==(cc::bitset<N> const& b) const
+    {
+        for (auto i : cc::indices_of(N))
+            if (entries.contains(i) != b.is_set(i))
+                return false;
+        return true;
+    }
+    bool operator!=(cc::bitset<N> const& b) const
+    {
+        for (auto i : cc::indices_of(N))
+            if (entries.contains(i) != b.is_set(i))
+                return true;
+        return false;
+    }
+
+    friend bool operator==(cc::bitset<N> const& a, reference_set const& b) { return b == a; }
+    friend bool operator!=(cc::bitset<N> const& a, reference_set const& b) { return b != a; }
+};
+
+} // namespace
+
+MONTE_CARLO_TEST("cc::bitset mct")
+{
+    addValue("int 0", 0);
+    addValue("int 1", 1);
+    addValue("int -1", -1);
+    addValue("int 10", 10);
+    addValue("int -10", -10);
+    addValue("int 63", 63);
+    addValue("int 64", 64);
+    addValue("int 65", 65);
+
+    addOp("rng int 10", [](tg::rng& rng) { return uniform(rng, -10, 10); });
+    addOp("rng int 100", [](tg::rng& rng) { return uniform(rng, -100, 100); });
+    addOp("rng int 200", [](tg::rng& rng) { return uniform(rng, -200, 200); });
+
+    auto setup_type = [this](auto bs)
+    {
+        using bitset_t = decltype(bs);
+
+        setPrinter<bitset_t>(
+            [](bitset_t const& b)
+            {
+                auto s = cc::string::filled(b.size(), '0');
+                for (auto i : cc::indices_of(b.size()))
+                    if (b[b.size() - i - 1])
+                        s[i] = '1';
+                return s;
+            });
+
+        auto const idx_in_range = [s = int(bs.size())](int i) { return 0 <= i && i < s; };
+
+        addValue("default", bitset_t{});
+        addValue("zeroes", bitset_t::zeroes());
+        addValue("ones", bitset_t::ones());
+        addValue("filled(true)", bitset_t::filled(true));
+        addValue("filled(false)", bitset_t::filled(false));
+
+        addOp("op~", [](bitset_t const& b) { return ~b; });
+        addOp("op|", [](bitset_t const& b0, bitset_t const& b1) { return b0 | b1; });
+        addOp("op&", [](bitset_t const& b0, bitset_t const& b1) { return b0 & b1; });
+        addOp("op^", [](bitset_t const& b0, bitset_t const& b1) { return b0 ^ b1; });
+        addOp("op<<", [](bitset_t const& b0, int b) { return b0 << b; });
+        addOp("op>>", [](bitset_t const& b0, int b) { return b0 >> b; });
+
+        addOp("op|=", [](bitset_t& b0, bitset_t const& b1) { b0 |= b1; });
+        addOp("op&=", [](bitset_t& b0, bitset_t const& b1) { b0 &= b1; });
+        addOp("op^=", [](bitset_t& b0, bitset_t const& b1) { b0 ^= b1; });
+        addOp("op<<=", [](bitset_t& b0, int b) { b0 <<= b; });
+        addOp("op>>=", [](bitset_t& b0, int b) { b0 >>= b; });
+
+        addOp("any", [](bitset_t const& b0) { return b0.any(); });
+        addOp("all", [](bitset_t const& b0) { return b0.all(); });
+
+        addOp("is_set", [](bitset_t const& b0, int b) { return b0.is_set(b); }).when(idx_in_range);
+        addOp("is_unset", [](bitset_t const& b0, int b) { return b0.is_unset(b); }).when(idx_in_range);
+        addOp("op[]", [](bitset_t const& b0, int b) { return b0[b]; }).when(idx_in_range);
+
+        addOp("clear", [](bitset_t& b0) { b0.clear(); });
+        addOp("set", [](bitset_t& b0, int b) { b0.set(b); }).when(idx_in_range);
+        addOp("unset", [](bitset_t& b0, int b) { b0.unset(b); }).when(idx_in_range);
+        addOp("toggle", [](bitset_t& b0, int b) { b0.toggle(b); }).when(idx_in_range);
+    };
+
+    setup_type(cc::bitset<64>{});
+    setup_type(reference_set<64>{});
+    testEquivalence<cc::bitset<64>, reference_set<64>>();
+
+    setup_type(cc::bitset<128>{});
+    setup_type(reference_set<128>{});
+    testEquivalence<cc::bitset<128>, reference_set<128>>();
+
+    setup_type(cc::bitset<63>{});
+    setup_type(reference_set<63>{});
+    testEquivalence<cc::bitset<63>, reference_set<63>>();
+
+    setup_type(cc::bitset<65>{});
+    setup_type(reference_set<65>{});
+    testEquivalence<cc::bitset<65>, reference_set<65>>();
+
+    setup_type(cc::bitset<130>{});
+    setup_type(reference_set<130>{});
+    testEquivalence<cc::bitset<130>, reference_set<130>>();
+}
+
+TEST("cc::bitset cases")
+{
+    {
+        auto bs = cc::bitset<64>::zeroes();
+        bs.toggle(63);
+        CHECK(bs.representation().words[0] == (size_t(1) << 63));
+    }
+    {
+        auto bs = cc::bitset<63>::ones();
+        bs = bs >> 63;
+        bs = ~bs;
+        CHECK(bs.all());
+    }
+}
